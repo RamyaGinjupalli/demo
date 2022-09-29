dafb6c1a9bb44cbea4ee1448a30131b841dfe90d
Ramya-199220 <Ramya-199220>
2022-09-29 10:57:31 +0530

    analyzer

---
+++ Analyzer.java
@@ -0,0 +1,8 @@
+package javaProject.student;
+
+public class Analyzer {
+	public static void main(String[] args) {
+	System.out.println("Hello 345");
+	}
+
+}
---
+++ Javaproject.java
@@ -0,0 +1,10 @@
+package javaProject.student;
+
+public class Javaproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("15");
+	}
+
+}
